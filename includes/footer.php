70293ab4bdb39da42549665b7d976e16d688f806
rohan <rohansakha343@gmail.com>
2022-01-15 09:38:07 +0545

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 <a href="https://www.instagram.com/shrestha_rohan001/">Rohan Sakha</a></strong>
+    </div>
+</footer>
